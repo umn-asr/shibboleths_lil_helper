3d57a86739c73a382853efe0b07d16df7ab73a5f
Joe Goggins <goggins@umn.edu>
2011-08-15 15:55:04 -0500

    lazy auth, aka authentication_optional now specified

--- lib/slh/models/site_path.rb
+++ lib/slh/models/site_path.rb
@@ -26,6 +26,17 @@ class Slh::Models::SitePath
   end
 
   def set(inst_var, inst_val)
-    puts "TODO, set #{inst_var}=#{inst_val}"
+    inst_val = inst_val.to_s if inst_val.kind_of?(Symbol)
+    if inst_val.kind_of?(String)
+      self.instance_eval <<-EOS,__FILE__,__LINE__
+        def #{inst_var}
+          '#{inst_val}'
+        end
+      EOS
+    elsif inst_val.kind_of? Proc
+      raise "Not implemented, someone should add if this is needed"
+    else
+      raise "don't know how to set with a #{inst_val.class.to_s} typped object, u had #{inst_val}"
+    end
   end
 end
