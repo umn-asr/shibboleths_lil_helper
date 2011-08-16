1941e654ad68b0a159d2b8e06e74237c58c63384
Joe Goggins <goggins@umn.edu>
2011-08-16 12:36:58 -0500

    :authentication_required_for_specific_users added for the IIS world, protect '/' working, enough IIS config generated now, need to move onto capistrano funtimes

--- lib/slh/models/site.rb
+++ lib/slh/models/site.rb
@@ -10,6 +10,12 @@ class Slh::Models::Site < Slh::Models::Base
   end
 
   def protect(site_path, *args, &block)
+    unless site_path.match(/^\//)
+      raise "protect statements must begin with a slash, aka /"
+    end
+    if site_path == '/' && !@paths.empty?
+      raise "If you want to protect the entire site, you must specify \"protect '/'\" before all other site path rules"
+    end
     @paths << Slh::Models::SitePath.new(site_path, *args, &block)
   end
 
@@ -25,11 +31,76 @@ class Slh::Models::Site < Slh::Models::Base
   # because the particular bit of XML includes 2 layers...it's not just
   # a function of the SitePath...
   #
+  # See https://wiki.shibboleth.net/confluence/display/SHIB2/NativeSPRequestMapHowTo
+  # for details on this logic
+  #
+  def iis_xml_payload_for_flavor(flavor)
+    if flavor == :authentication_optional
+      'authType="shibboleth" requireSession="false"'
+    elsif [:authentication_required,:authentication_required_for_specific_users].include?(flavor)
+      'authType="shibboleth" requireSession="true"'
+    else 
+      raise "No iis_xml_payload_for_flavor flavor=#{flavor}"
+    end
+  end
   def to_iis_directive(*args)
-    options = args.extract_options!
-    @strategy = options[:strategy]
-    @host = options[:host]
-    @site = options[:site]
+    host_begin = ''
+    host_end = '</Host>'
+    path_strings = []
+    if self.paths.first.name == '/'
+      host_begin = <<-EOS
+<!-- Shibboleths Lil Helper flavor=#{self.paths.first.flavor} protection for entire site -->
+<Host name="#{self.name}" #{self.iis_xml_payload_for_flavor(self.paths.first.flavor)} redirectToSSL="443" >"
+EOS
+    else
+      host_begin = "<Host name=\"#{self.name}\" redirectToSSL=\"443\" >"
+    end
+    self.paths.each do |p|
+      next if p.name == '/' # Already dealt with/baked into the <Host> Xml
+      if p.flavor == :authentication_required_for_specific_users
+        path_strings << <<-EOS
+          <!-- Shibboleths Lil Helper flavor=#{p.flavor} -->
+          <Path name="#{p.name}" #{self.iis_xml_payload_for_flavor(p.flavor)}>
+            <AccessControl>
+              <AND>
+                <Rule require="user">#{p.specific_users.join(' ')}</Rule>
+              </AND>
+            </AccessControl>
+          </Path>
+        EOS
+      else
+        path_strings << "<Path name=\"#{p.name}\" #{self.iis_xml_payload_for_flavor(p.flavor)} />"
+      end
+    end
+    return "#{host_begin}\n#{path_strings.join("\n")}\n#{host_end}"
+    # EXAMPLE1
+    # for_site 'asr-web-dev2.oit.umn.edu' do
+    #   set :site_id, "1351780944"
+    #   protect '/'
+    # EXAMPLE2
+    # for_site 'asr-web-dev2.oit.umn.edu' do
+    #   set :site_id, "1351780944"
+    #   protect '/' do
+    #     set :flavor :authentication_optional
+   # INTERPRETATION
+    #   Single auth rule for entire site, no <Path> nodes needed
+    # if self.paths.length == 1 && self.paths.first.name == '/'
+    #   host_attrs = flavors[self.paths.first.flavor]
+    #   raise "No iis config info for #{self.paths.first.flavor}" if host_attrs.blank?
+    #   return <<-EOS
+    #     <Host name="#{self.name}" #{host_attrs} redirectToSSL="443" />
+    #   EOS
+    # else
+    #   # setup the opening host tag
+    #   s = <<-EOS
+    #     <Host name="#{self.name}" redirectToSSL="443">
+    #   EOS
+    #   self.paths.each do |p|
+    #     if p.name == '/'
+
+    #     end
+    #   end
+    # end
     "TODO, do magic w #{@strategy} #{@host} #{@site}"
     # ERB.new(self.iis_directive_template_file_content).result(binding)
   end
--- lib/slh/models/site_path.rb
+++ lib/slh/models/site_path.rb
@@ -1,9 +1,11 @@
 class Slh::Models::SitePath < Slh::Models::Base
   attr_reader :name
-  attr_accessor :flavor
+  attr_accessor :flavor,
+    :specific_users # for usage when the flavor is :authentication_required_for_specific_users
   def initialize(site_path,*args, &block)
     @name = site_path
     @flavor = :authentication_required
+    @specific_users = []
     options = args.extract_options!
     if block_given?
       self.instance_eval(&block)
