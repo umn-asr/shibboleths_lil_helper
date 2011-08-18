188ab36d95548d1f8e19e2722c4154455bb63e22
Joe Goggins <goggins@umn.edu>
2011-08-18 17:28:00 -0500

    adding a tweak for Chris

--- t3.sh
+++ t3.sh
@@ -2,4 +2,8 @@
 ./bin/slh generate
 ./bin/slh metadata
 
-# TODO: COPY THE GENERATED STUFF OVER TO shibboleth_deployer
+# YO CHRIS--UNCOMMENT THIS...
+# FOR DEV with shibboleth_deployer, copy this regenerated config
+# over to that repo
+# rm -rf ../shibboleth_deployer/shibboleths_lil_helper
+# cp -r shibboleths_lil_helper ../shibboleth_deployer/
