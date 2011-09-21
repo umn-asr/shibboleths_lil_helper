99368c31783ae30d7a66da307f26a9574a22ae4f
Chris Dinger <ding0057@umn.edu>
2011-09-21 10:23:23 -0500

    Added some help messages after generate/metadata commands to inform users about external dependencies

--- lib/slh/cli/generate.rb
+++ lib/slh/cli/generate.rb
@@ -5,7 +5,12 @@ class Slh::Cli::Generate < Slh::Cli::CommandBase
   def perform_action
     Slh.strategies.each do |s|
       s.generate_config
-      Slh::Cli.instance.output "Generated config for strategy \"#{s.name.to_s}\" in #{s.config_dir}"
+      Slh::Cli.instance.output <<-EOS
+Generated config for strategy \"#{s.name.to_s}\" in #{s.config_dir}
+
+You MUST deploy these genereated files to your web server before running `slh metadata`
+
+EOS
     end
   end
 end
