a6f6a9e22a8a5d17ba21db8c779cb922084c5ae5
Joe Goggins <goggins@umn.edu>
2011-09-23 11:01:57 -0500

    fetch_metadata works

--- lib/slh/cli/fetch_metadata.rb
+++ lib/slh/cli/fetch_metadata.rb
@@ -7,6 +7,9 @@ class Slh::Cli::FetchMetadata < Slh::Cli::CommandBase
       strategy.hosts.each do |host|
         host.sites.each do |site|
           puts "Fetching metadata for #{site.name}"
+          site_dir = File.join(strategy.config_dir_for_host(host),site.name.to_s)
+          FileUtils.mkdir_p(site_dir)
+          File.open(File.join(site_dir,'fetched_metadata.xml'),'w') {|f| f.write(site.metadata) }
         end
       end
     end
