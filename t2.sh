31024b9911f3ee849f4a89abf4ce83cb543222af
Joe Goggins <goggins@umn.edu>
2011-11-01 15:56:38 -0500

    Version bump to 1.0.0

--- t2.sh
+++
@@ -1,3 +0,0 @@
-./bin/slh initialize -f
-./bin/slh generate
-./bin/slh metadata
--- t3.sh
+++
@@ -1,9 +0,0 @@
-./bin/slh initialize -f
-./bin/slh generate
-./bin/slh metadata
-
-# YO CHRIS--UNCOMMENT THIS...
-# FOR DEV with shibboleth_deployer, copy this regenerated config
-# over to that repo
-# rm -rf ../shibboleth_deployer/shibboleths_lil_helper
-# cp -r shibboleths_lil_helper ../shibboleth_deployer/
